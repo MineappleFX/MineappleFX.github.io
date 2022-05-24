c6b7099e5b9e41bba49eb1523a12f84a1f23bd75
MineappleFX <71654243+MineappleFX@users.noreply.github.com>
2022-05-24 17:23:41 +0200

    Update accountAux.lua

--- accountAux.lua
+++ accountAux.lua
@@ -3,7 +3,7 @@ local internet = require("internet")
 local filesystem = require("filesystem")
 local shell = require("shell")
 
-local urlAccount = "http://217.238.235.143:80/accountCreation"
+local urlAccount = "http://amongus.tk/accountCreation"
 local webIdPath = "/home/myaenetwork/webIdentification.txt"
 local workDirectory = "/home/myaenetwork/"
 local newDirectory = "/home/myaenetwork"
